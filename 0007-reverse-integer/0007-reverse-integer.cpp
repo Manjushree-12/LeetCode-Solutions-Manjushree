27c67bf880cf18faf607bb618cd08c2a45dd2a3f
Manjushree R G <manjushreerajendiran@gmail.com>
2025-09-18 20:18:15 +0530

    Time: 5 ms (19.97%), Space: 8.1 MB (99.98%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long  y=0;
+        while(x!=0)
+        {
+            y += x%10;
+            y = y*10;
+            x = x/10;
+
+        }
+        y=y/10;
+        if(y>INT_MAX || y<INT_MIN) return 0;
+                return y; 
+        
+    }
+};
