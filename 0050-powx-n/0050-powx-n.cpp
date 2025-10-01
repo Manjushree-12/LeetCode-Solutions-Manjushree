a9cf59e362895aa5e49d063283226ee05ca1ce36
Manjushree R G <manjushreerajendiran@gmail.com>
2025-10-01 22:25:42 +0530

    Time: 0 ms (100%), Space: 8.5 MB (71.31%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double ans =1.0;
+        long long nn = n;
+        if(n<0)
+        {
+            nn = -1LL*n;
+        }
+        while(nn>0)
+        {
+            if(nn%2==1)
+            {
+                ans *=x;
+                nn -=1;
+            }
+            if(nn%2==0)
+            {
+                x*=x;
+                nn /=2;
+            }
+        }
+        if(n<0)
+        {
+            ans = double(1.0)/ans;
+        }
+        return ans;
+    }
+};
